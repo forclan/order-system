c7a6f0634f8925833ff609d07fd7a1ac9ffb9b2c
forclan <hxplian1992@163.com>
2016-07-06 11:24:49 +0800

    add const folder, put export values in it;

--- src/reducer/DishInitialState.js
+++ src/reducer/DishInitialState.js
@@ -1,4 +1,4 @@
-export const initState = [
+const initialState = [
   {
     id: 1,
     name: 'apple',
@@ -24,3 +24,5 @@ export const initState = [
     name: '1123123',
     price: 23,
   }];
+
+export default initialState;
