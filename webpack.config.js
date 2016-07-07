af915dc24ca95a2efc1c6566fe4c7ec609d5ab4a
forclan <hxplian1992@163.com>
2016-07-07 13:56:45 +0800

    change /static to /lib

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
     filename: '[name].js',
     // library: 'shared-components',
     libraryTarget: 'umd',
-    publicPath: '/static/',
+    publicPath: '/lib/',
   },
   plugins,
   module: {
